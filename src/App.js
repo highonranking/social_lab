3eaed8d6c0200e4540f2b8e2a5b11871c50acf38
abhinav dixit <abhinavdixit2306@gmail.com>
2020-04-23 09:39:44 +0530

    component 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap'; 
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes : DISHES
+    };
+  }
+
+
+render() {
   return (
     <div>
 
@@ -13,12 +24,13 @@ function App() {
       <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
       </div>
       </Navbar>
-      
-      
-      <Menu />
-      
+
+
+      <Menu dishes = {this.state.dishes} />
+
     </div>
   );
+}
 }
 
 export default App;
