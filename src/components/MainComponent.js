497dcef692ef415968dafe0bcbdfd0ef466352cf
abhinav dixit <abhinavdixit2306@gmail.com>
2020-04-29 15:56:13 +0530

    Assignment 2 complete

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
+import About from './AboutComponent';
 
 import { Switch, Route, Redirect } from 'react-router-dom';
 
@@ -54,6 +55,7 @@ const DishWithId = ({match}) => {
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
+          <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
           <Redirect to="/home" />
         </Switch>
 
