b103daf2d9f7f04d6e984b1a61fe93ba94bc7186
abhinav dixit <abhinavdixit2306@gmail.com>
2020-04-30 09:14:35 +0530

    controlled formm 1

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,3 +1,5 @@
+//jshint esversion:6
+
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
