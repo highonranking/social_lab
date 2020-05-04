951333f44a141c7e2fd4a745dd1202df896bd6b0
abhinav dixit <abhinavdixit2306@gmail.com>
2020-05-04 09:34:07 +0530

    fetch handling errors

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -20,8 +20,22 @@ export const fetchDishes = () => (dispatch) => {
         dispatch(dishesLoading(true));
 
         return fetch(baseUrl + 'dishes')
-         .then(response => response.json())
-         .then(dishes => dispatch(addDishes(dishes)));
+      .then(response => {
+          if (response.ok) {
+            return response;
+          } else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+          }
+        },
+        error => {
+              var errmess = new Error(error.message);
+              throw errmess;
+        })
+      .then(response => response.json())
+      .then(dishes => dispatch(addDishes(dishes)))
+      .catch(error => dispatch(dishesFailed(error.message)));
 
 
 };
@@ -43,8 +57,22 @@ export const addDishes = (dishes) => ({
 
 export const fetchComments = () => (dispatch) => {
     return fetch(baseUrl + 'comments')
-    .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)));
+    .then(response => {
+          if (response.ok) {
+            return response;
+          } else {
+            var error = new Error('Error ' + response.status + ': ' + response.statusText);
+            error.response = response;
+            throw error;
+          }
+        },
+        error => {
+              var errmess = new Error(error.message);
+              throw errmess;
+        })
+      .then(response => response.json())
+      .then(comments => dispatch(addComments(comments)))
+      .catch(error => dispatch(commentsFailed(error.message)));
 };
 
 export const commentsFailed = (errmess) => ({
@@ -60,10 +88,24 @@ export const addComments = (comments) => ({
 export const fetchPromos = () => (dispatch) => {
 
     dispatch(promosLoading());
-
     return fetch(baseUrl + 'promotions')
-    .then(response => response.json())
-    .then(promos => dispatch(addPromos(promos)));
+       .then(response => {
+           if (response.ok) {
+             return response;
+           } else {
+             var error = new Error('Error ' + response.status + ': ' + response.statusText);
+             error.response = response;
+             throw error;
+           }
+         },
+         error => {
+               var errmess = new Error(error.message);
+               throw errmess;
+         })
+       .then(response => response.json())
+       .then(promos => dispatch(addPromos(promos)))
+       .catch(error => dispatch(promosFailed(error.message)));
+
 };
 
 export const promosLoading = () => ({
